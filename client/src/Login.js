56e8c910b3b4898190fa9740638a13822d119f20
Cihem <cihemzine@gmail.com>
2021-05-04 14:25:18 +0100

    adding login form

--- client/src/Login.js
+++ client/src/Login.js
@@ -6,6 +6,9 @@ export default class Login extends Component {
         return (
             <div>
                 <h2><span className="ever">Ever</span><span className="learn">Learn</span></h2>
+                <input placeholder="email" type="text" /><br></br><br></br>
+                <input placeholder="password" type="password" /><br></br><br></br>
+                <button className="loginButton">Sign in</button>
             </div>
         )
     }
